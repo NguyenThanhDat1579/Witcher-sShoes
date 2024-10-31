a747ea351cf935f7c190d813a2a7f56ec7148705
HohLeeSheet <tiiensibo2706@gmail.com>
2024-10-31 13:31:42 +0700

    push du lieu, lay du lieu

--- app/src/main/java/com/example/witchersshoes/classes/ToDo.java
+++ app/src/main/java/com/example/witchersshoes/classes/ToDo.java
@@ -3,18 +3,52 @@ package com.example.witchersshoes.classes;
 import java.util.HashMap;
 
 public class ToDo {
-    public String id, title, content;
-    public ToDo (String id , String title, String content){
-        this.id=id;
-        this.title=title;
-        this.content=content;
-    }
-    public HashMap<String, Object> convertHashMap(){
-        HashMap<String,Object> map = new HashMap<String,Object>();
-        map.put("id",id);
-        map.put("title",title);
-        map.put("content",content);
-        return map;
+    private String id;
+    private String title;
+    private String content;
 
+    // Constructor không tham số (yêu cầu để Firestore có thể deserialize dữ liệu)
+    public ToDo() {
+    }
+
+    // Constructor có tham số
+    public ToDo(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    // Các getter và setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // Phương thức để chuyển đối tượng thành HashMap nếu cần
+    public HashMap<String, Object> convertHashMap() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        map.put("title", title);
+        map.put("content", content);
+        return map;
     }
 }
