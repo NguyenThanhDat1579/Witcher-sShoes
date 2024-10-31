1bd53b4249615cc7493c12bb087106fe06e6fead
HohLeeSheet <tiiensibo2706@gmail.com>
2024-10-31 13:31:42 +0700

    push du lieu, lay du lieu

--- app/src/main/java/com/example/witchersshoes/MainActivity.java
+++ app/src/main/java/com/example/witchersshoes/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.witchersshoes;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,18 +13,13 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.witchersshoes.classes.ToDo;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore database;
-    TextView txtNameShoes, txtSize, txtCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +27,14 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        txtNameShoes = findViewById(R.id.shoesName);
-        txtSize = findViewById(R.id.size);
-        txtCategory = findViewById(R.id.category);
-
         // Khởi tạo FirebaseFirestore
         database = FirebaseFirestore.getInstance();
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         String id = UUID.randomUUID().toString();
         String title = "tieu de ne";
@@ -48,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
         ToDo toDo = new ToDo(id, title, content);
         HashMap<String, Object> map = toDo.convertHashMap();
-        //push du lieu len
+
         database.collection("ToDo").document(id)
                 .set(map)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -63,51 +57,5 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "that bai", Toast.LENGTH_SHORT).show();
                     }
                 });
-/*
-        //lay du lieu ve
-        database.collection("ToDo")
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            // Chuyển dữ liệu thành một đối tượng ToDo hoặc xử lý tùy ý
-                            ToDo toDo2 = document.toObject(ToDo.class);
-                            String title2 = toDo.getTitle();
-                            String content2 = toDo.getContent();
-
-                            txtNameShoes.setText(title2);
-                            // Hiển thị dữ liệu
-                            Toast.makeText(MainActivity.this, "Title: " + title2 + ", Content: " + content2, Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        Toast.makeText(MainActivity.this, "Lỗi khi lấy dữ liệu", Toast.LENGTH_SHORT).show();
-                    }
-                });
-*/
-        database.collection("Shoes").document("s1")
-                .get()
-                .addOnSuccessListener(documentSnapshot -> {
-                   if(documentSnapshot.exists()){
-                       String name = documentSnapshot.getString("name");
-                       String size = documentSnapshot.getString("size");
-                       // Liên kết với loại giày
-                       DocumentReference typeRef = documentSnapshot.getDocumentReference("category");
-
-                       txtNameShoes.setText(name);
-                       txtSize.setText(size);
-                       if(typeRef!=null){
-                           typeRef.get().addOnSuccessListener(typeSnapshot->{
-                               if(typeSnapshot.exists()){
-                                   String category = typeSnapshot.getString("name");
-                                   txtNameShoes.setText(name);
-                                   txtSize.setText(size);
-                                   txtCategory.setText(category);
-                               }
-                           });
-                       }
-                    }
-                })
-                .addOnFailureListener(e -> Log.w("Firestore", "Error getting shoe data", e));
-
     }
 }
