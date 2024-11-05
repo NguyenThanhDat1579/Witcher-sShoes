b98d2ecd5ab20a6a93b32d5f904e2658c1742189
toanpt301 <phamthanhtoan301bt@gmail.com>
2024-11-05 22:25:21 +0700

    testtoan

--- app/src/main/java/com/example/witchersshoes/DangKy.java
+++ app/src/main/java/com/example/witchersshoes/DangKy.java
@@ -20,5 +20,7 @@ public class DangKy extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//        hihihihihihihi
     }
 }
