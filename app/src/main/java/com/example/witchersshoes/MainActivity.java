4f85b3661475359d9faad0c1531a6b94df8a29c7
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2024-11-06 09:44:12 +0700

    dang nhap va dang ky

--- app/src/main/java/com/example/witchersshoes/MainActivity.java
+++ app/src/main/java/com/example/witchersshoes/MainActivity.java
@@ -14,6 +14,7 @@ import com.example.witchersshoes.classes.ToDo;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.HashMap;
 import java.util.UUID;
@@ -26,36 +27,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        // Khởi tạo FirebaseFirestore
-        database = FirebaseFirestore.getInstance();
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+//        // Khởi tạo FirebaseFirestore
+//        database = FirebaseFirestore.getInstance();
+//        String id = UUID.randomUUID().toString();
+//        String title = "tieu de ne";
+//        String content = "noi dung ne";
+//
+//        ToDo toDo = new ToDo(id, title, content);
+//        HashMap<String, Object> map = toDo.convertHashMap();
+//        //push du lieu len
+//        database.collection("ToDo").document(id)
+//                .set(map)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void unused) {
+//                        Toast.makeText(MainActivity.this, "thanh cong", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(MainActivity.this, "that bai", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//        //lay du lieu ve
+//        database.collection("ToDo")
+//                .get()
+//                .addOnCompleteListener(task -> {
+//                    if (task.isSuccessful()) {
+//                        for (QueryDocumentSnapshot document : task.getResult()) {
+//                            // Chuyển dữ liệu thành một đối tượng ToDo hoặc xử lý tùy ý
+//                            ToDo toDo2 = document.toObject(ToDo.class);
+//                            String title2 = toDo.getTitle();
+//                            String content2 = toDo.getContent();
+//
+//                            // Hiển thị dữ liệu
+//                            Toast.makeText(MainActivity.this, "Title: " + title2 + ", Content: " + content2, Toast.LENGTH_SHORT).show();
+//                        }
+//                    } else {
+//                        Toast.makeText(MainActivity.this, "Lỗi khi lấy dữ liệu", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
 
-        String id = UUID.randomUUID().toString();
-        String title = "tieu de ne";
-        String content = "noi dung ne";
-
-        ToDo toDo = new ToDo(id, title, content);
-        HashMap<String, Object> map = toDo.convertHashMap();
-
-        database.collection("ToDo").document(id)
-                .set(map)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Toast.makeText(MainActivity.this, "thanh cong", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "that bai", Toast.LENGTH_SHORT).show();
-                    }
-                });
     }
 }
