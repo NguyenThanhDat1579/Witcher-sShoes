426b7521db7bb4a664d9e480dfb057b59d46e0ff
HohLeeSheet <tiiensibo2706@gmail.com>
2024-11-07 07:57:32 +0700

    giao dien trang chu

--- app/src/main/java/com/example/witchersshoes/TrangChu.java
+++ app/src/main/java/com/example/witchersshoes/TrangChu.java
@@ -7,6 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.witchersshoes.classes.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrangChu extends AppCompatActivity {
 
@@ -20,5 +27,18 @@ public class TrangChu extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // Tạo danh sách sản phẩm mẫu
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Basas Workaday", "₫2,500,000", R.drawable.shoes1));
+        products.add(new Product("Basas RAW", "₫1,800,000", R.drawable.shoes3));
+        products.add(new Product("Basas Evergreen", "₫2,500,000", R.drawable.shoes4));
+        products.add(new Product("Basas Workaday", "₫2,500,000", R.drawable.shoes1));
+
+        // Cài đặt LayoutManager và Adapter cho RecyclerView
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2)); // 2 cột
+        ProductAdapter adapter = new ProductAdapter(products);
+        recyclerView.setAdapter(adapter);
     }
 }
